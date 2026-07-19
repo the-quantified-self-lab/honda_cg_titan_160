0a861e6b6f235bef985b3f748103c5ab6b163b9f
the-quantified-self-lab <juliano.gnoatto@outlook.com>
2026-07-19 16:57:03 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = "cg160-titan-v2";
+const ASSETS = [
+  "./","./index.html","./manifest.webmanifest",
+  "./icon-192.png","./icon-512.png","./icon-maskable-512.png"
+];
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ).then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", e => {
+  const url = new URL(e.request.url);
+  // Só cuida dos arquivos do próprio app. Google (login/planilha) sempre vai à rede.
+  if (e.request.method !== "GET" || url.origin !== self.location.origin) return;
+  e.respondWith(
+    caches.match(e.request).then(hit =>
+      hit || fetch(e.request).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+        return res;
+      }).catch(() => caches.match("./index.html"))
+    )
+  );
+});
